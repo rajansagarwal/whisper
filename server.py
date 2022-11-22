750bf2131c4d7bacfbf9fdd5ecb6c7df1008ea7d
Rajan Agarwal <rajan.ag005@gmail.com>
2022-11-22 12:34:41 -0500

    hard-coded search

--- server.py
+++ server.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template, request
 import whisper
 import spacy
+
 # import nltk
 # from gensim.summarization import summarize
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html', name="Upload Files to Transcribe")
+
 
 @app.route('/analyze/', methods=['POST', 'GET'])
 def my_link():
@@ -30,16 +33,18 @@ def my_link():
 
         for i in range(len(text1.ents)):
             array.append(
-                {'text': text1.ents[i].label_, 
-                'label': text1.ents[i].text
-            })
-        
+                {'text': text1.ents[i].label_,
+                 'label': text1.ents[i].text
+                 })
+
         # nltk.download('punkt')
         # nltk.download('stopwords')
 
         # summary = summarize(out, ratio=0.2, split=False)
 
-        return render_template('index.html', out=out['text'], final=str(final)[1:-1], name=file, array=array, data=form_data)
+        return render_template('index.html', out=out['text'], final=str(final)[1:-1], name=file, array=array,
+                               data=form_data)
+
 
 if __name__ == '__main__':
-  app.run(debug=True)
+    app.run(debug=True)
